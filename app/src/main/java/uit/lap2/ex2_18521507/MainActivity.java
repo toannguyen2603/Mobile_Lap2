c77f2761210ae9423d14e8d6b976f65d5637a4cb
Nguyễn Hữu Toàn <huutoan2603@gmail.com>
2022-03-29 09:36:10 +0700

    finish execrise 1

---
+++ app/src/main/java/uit/lap2/ex2_18521507/MainActivity.java
@@ -0,0 +1,44 @@
+package uit.lap2.ex2_18521507;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+public class MainActivity extends AppCompatActivity {
+    ImageView image;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        image = findViewById(R.id.imageView);
+        registerForContextMenu(image);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater image = getMenuInflater();
+        image.inflate(R.menu.menu_option,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        Toast.makeText(MainActivity.this, item.getTitle(), Toast.LENGTH_SHORT).show();
+        return true;
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        MenuInflater image = getMenuInflater();
+        image.inflate(R.menu.menu_context,menu);
+    }
+
+
+}
